4a2f1c3086ab94a55faa80f62e927d029c004b18
Jiří Schejbal <jirkaschej@seznam.cz>
2020-06-08 17:18:50 +0200

    Updates

---
+++ precache-manifest.7925f3ff314ee565b28fff7c38f38178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa99aa2253877b5a66842d7937e1f44e",
+    "url": "/ohm_sender/index.html"
+  },
+  {
+    "revision": "0cf9c99cc2999c21e65b",
+    "url": "/ohm_sender/static/css/main.e032d9bf.chunk.css"
+  },
+  {
+    "revision": "d9c756e37ebbb6e2eeac",
+    "url": "/ohm_sender/static/js/2.e546f19b.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/ohm_sender/static/js/2.e546f19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf9c99cc2999c21e65b",
+    "url": "/ohm_sender/static/js/main.5d7b5ffc.chunk.js"
+  },
+  {
+    "revision": "a3683388b827b11c6706",
+    "url": "/ohm_sender/static/js/runtime-main.4b61e102.js"
+  }
+]);
